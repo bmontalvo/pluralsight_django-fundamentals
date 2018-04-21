64000e760550bdb68a21f1457a3637b833ce097c
Bobby Montalvo <bobby.montalvo@gmail.com>
2018-04-21 16:33:50 -0700

    Added status field to gameplay.Game model. Performed migration.

--- tictactoe/gameplay/models.py
+++ tictactoe/gameplay/models.py
@@ -8,6 +8,7 @@ class Game(models.Model):
 
     start_time = models.DateTimeField(auto_now_add=True)
     last_active = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=1, default='F')
 
 
 class Move(models.Model):
