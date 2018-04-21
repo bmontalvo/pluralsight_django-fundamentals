eec231af57dffcf8e9f0d6edcb77ab01d394cdcc
Bobby Montalvo <bobby.montalvo@gmail.com>
2018-04-21 15:23:24 -0700

    Created gameplay app with models. Performed db migration for new models.

---
+++ tictactoe/gameplay/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Game(models.Model):
+    first_player = models.ForeignKey(User, related_name="games_first_player", on_delete=models.CASCADE)
+    second_player = models.ForeignKey(User, related_name="games_second_player", on_delete=models.CASCADE)
+
+    start_time = models.DateTimeField(auto_now_add=True)
+    last_active = models.DateTimeField(auto_now=True)
+
+
+class Move(models.Model):
+    x = models.IntegerField()
+    y = models.IntegerField()
+    comment = models.CharField(max_length=300, blank=True)
+    by_first_player = models.BooleanField()
+
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+
